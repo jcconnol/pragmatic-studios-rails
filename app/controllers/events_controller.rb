3083d7aa7756e99c2a7508f20e810d254921c1fb
John Connolly <jcconnol4@gmail.com>
2023-02-09 21:21:18 -0600

    add create event endpoint and page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,4 +12,30 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
 
   end
+
+  def update
+    @event = Event.find(params[:id])
+
+    @event.update(event_params)
+
+    redirect_to event_path
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+
+    redirect_to event_path(@event)
+  end
+
+  private
+
+  def event_params
+    params.require(:event)
+      .permit(:name, :description, :location, :price, :starts_at)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root "events#index"
 
-  get "events" => "events#index"
-  get "events/:id" => "events#show", as: "event"
-  get "events/:id/edit" => "events#edit", as: "event_edit"
+  resources :events # Rails makes all routes for event with one line
+
+  # get "events" => "events#index"
+  # post "events/new" => "events#create"
+  # get "events/:id" => "events#show", as: "event"
+  # get "events/:id/edit" => "events#edit", as: "edit_event"
+  # patch "events/:id" => "events#update"
+
 end
