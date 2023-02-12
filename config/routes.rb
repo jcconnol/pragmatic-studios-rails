ed6e594c3dc446530449a46e5c22f53f356cd36a
John Connolly <jcconnol4@gmail.com>
2023-02-12 13:50:32 -0600

    add sign up and stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :users
   root "events#index"
 
   resources :events do # Rails makes all routes for event with one line
     resources :registrations #makes events/id/registrations
   end
+
+  resources :users
+  get "signup" => "users#new"
 
   # get "events" => "events#index"
   # post "events/new" => "events#create"
