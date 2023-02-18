7635fb51b679fe59e13deea05780419b9256b8e6
John Connolly <35578555+jcconnol@users.noreply.github.com>
2023-02-18 09:26:57 -0600

    add stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: "You can edit!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy(user_params)
+    redirect_to events_url, alert: "You gone boii!"
+  end
+
   private
 
   def user_params
